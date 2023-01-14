2542cad0f664d0434cce78c2cb86f035b41d4a8c
dniero95 <darioniero@icloud.com>
2023-01-14 22:40:16 +0100

    add diff, prod, div operations endpoint

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -12,5 +12,18 @@ def get_root():
 def get_root(num1:float, num2:float):
     return {f"sum({num1}, {num2})":f"{num1+num2}"}
 
+@app.get('api/calculator/diff/{num1}/{num2}')
+def get_root(num1:float, num2:float):
+    return {f"diff({num1}, {num2})":f"{num1-num2}"}
+
+@app.get('api/calculator/prod/{num1}/{num2}')
+def get_root(num1:float, num2:float):
+    return {f"prod({num1}, {num2})":f"{num1*num2}"}
+
+@app.get('api/calculator/div/{num1}/{num2}')
+def get_root(num1:float, num2:float):
+    return {f"dif({num1}, {num2})":f"{num1/num2}"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=5000), log_level="info")
