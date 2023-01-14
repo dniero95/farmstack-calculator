9845041b0f40375aa1b9f046926b1d07bae7fc06
dniero95 <darioniero@icloud.com>
2023-01-14 22:38:15 +0100

    fix path

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 def get_root():
     return {"message":"welcome in the calculator"}
 
-@app.get('/calculator/{num1}/{num2}')
+@app.get('api/calculator/sum/{num1}/{num2}')
 def get_root(num1:float, num2:float):
     return {f"sum({num1}, {num2})":f"{num1+num2}"}
 
